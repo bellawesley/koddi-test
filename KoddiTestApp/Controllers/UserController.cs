72157e63e8d0ff01d22cfda2cec2a2bc4df4cd77
ISABELLA WESLEY <iwesley@email.sc.edu>
2018-03-13 22:09:07 -0400

    Add project files.

---
+++ KoddiTestApp/Controllers/UserController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KoddiTestApp.Models;
+
+namespace KoddiTestApp.Controllers
+{
+    public class UserController : Controller
+    {
+        // Registration action
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        // Registration POST action
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Registration(User user)
+        {
+            bool Status = false;
+            string message = "";
+
+            // Model Validation
+            if (ModelState.IsValid)
+            {
+                // Email exist already check
+                var IsExist = EmailExists(user.EmailID);
+                if (IsExist)
+                {
+                    ModelState.AddModelError("EmailExists", "Email already exists");
+                    return View(user);
+                }
+                // End email exist check
+
+                // Password hashing
+                user.Password = Crypto.Hash(user.Password);
+                user.ConfirmPassword = Crypto.Hash(user.ConfirmPassword);
+                // End password hashing
+
+                // Save data to DB
+                using (MyDatabaseEntities db =  new MyDatabaseEntities())
+                {
+                    db.Users.Add(user);
+                    db.SaveChanges();
+                    message = "Account successfully created.";
+                    Status = true;
+                }
+                // End save data
+
+            }
+            else
+            {
+                message = "Invalid request";
+            }
+
+            ViewBag.Message = message;
+            ViewBag.Status = Status;
+
+         return View(user);
+        }
+
+        // Login
+
+        // Login POST action
+
+        // Logout
+
+        [NonAction]
+        public bool EmailExists(string emailID)
+        {
+            using (MyDatabaseEntities db = new MyDatabaseEntities())
+            {
+                var emailOverlap = db.Users.Where(a => a.EmailID == emailID).FirstOrDefault();
+                return emailOverlap == null ? false : true; // return false if no overlap (email does not exist)
+            }
+        }
+    }
+}
---
+++ KoddiTestApp/Models/Extended/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace KoddiTestApp.Models
+{
+    [MetadataType(typeof(UserMetadata))]
+    public partial class User
+    {
+        public string ConfirmPassword { get; set; } // (Additional field, not in MyDatabase)
+    }
+
+    public class UserMetadata
+    {
+        [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress)]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email ID is required")]
+        public string EmailID { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Password is required")]
+        [MinLength(8, ErrorMessage = "Minimum password length of 8 characters is required")]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Password confirmation is required")]
+        [Compare("Password", ErrorMessage = "Passwords must match")]
+        public string ConfirmPassword { get; set; }
+    }
+
+}
